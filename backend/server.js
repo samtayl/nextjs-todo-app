1a884c19b320ec4905dff40e4d7ab7334ee8eeea
samtayl <sam@samtayl.com>
2024-01-09 00:31:44 +0000

    refactor(backend): use single 'state' property in Server class instead of multiple state properties

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,18 @@ import {promisify} from 'util';
 import 'dotenv/config';
 import app from './app/index.js';
 
+const SERVER_STATES = {
+  STARTING: Symbol('STARTING'),
+  STARTED: Symbol('STARTED'),
+  STOPPING: Symbol('STOPPING'),
+  STOPPED: Symbol('STOPPED'),
+};
+
 class Server {
   server = createServer();
   listen = promisify(this.server.listen).bind(this.server);
   close = promisify(this.server.close).bind(this.server);
-  isStarting = false;
-  hasStarted = false;
+  state = SERVER_STATES.STOPPED;
   shouldStop = false;
 
   constructor() {
@@ -18,12 +24,11 @@ class Server {
   async start() {
     console.log('Starting server');
 
-    this.isStarting = true;
+    this.state = SERVER_STATES.STARTING;
 
     await this.listen(process.env.PORT || 8080, process.env.HOSTNAME || 'localhost');
 
-    this.isStarting = false;
-    this.hasStarted = true;
+    this.state = SERVER_STATES.STARTED;
 
     const {
       family: boundFamily,
@@ -46,21 +51,28 @@ class Server {
     try {
       console.log('Stopping server');
 
+      this.state = SERVER_STATES.STOPPING;
+
       await this.close();
+
+      this.state = SERVER_STATES.STOPPED;
 
       console.log('Server stopped');
     }
     catch (error) {
       if (error.code === 'ERR_SERVER_NOT_RUNNING') {
-        if (this.isStarting) {
+        if (this.state === SERVER_STATES.STARTING) {
           console.log('Server is starting. Will stop when started.');
 
           this.shouldStop = true;
         }
-        else if (this.hasStarted) {
+        else if (this.state === SERVER_STATES.STARTED) {
           console.log('Server has started. Will stop server.');
 
           await this.stop();
+        }
+        else if (this.state === SERVER_STATES.STOPPING) {
+          console.log('Server is already stopping');
         }
         else {
           console.log('Server has not started and is not starting');
